22abe91ef9ed88215152dfb5e57b88aba47ab837
Your Name <you@example.com>
2022-07-19 18:04:01 +0530

    event added

---
+++ src/components/home.vue
@@ -0,0 +1,40 @@
+<template>
+<!-- <h1>Name.....!{{Name}}</h1>
+<h1>Email.....!{{email}}</h1>
+<h1>Mobile ....!{{Mobile}}</h1>
+<h1>Mobile ....!{{Mobile==8888}}</h1>
+<h1>company....!{{getName('geoSpoc')}}</h1> -->
+<h1 v-on:mousemove="getConsole()">Home</h1>
+<button v-on:click="getData()">Click Me!</button>
+
+<h2>{{count}}</h2>
+
+</template>
+
+<script>
+export default {
+    name:'homePage',
+    data()
+    {
+         return{
+            count:0
+         }
+    },
+    methods:{
+getData()
+{
+  this.count=this.count+1;
+},
+getConsole()
+{
+    console.warn("function called");
+}
+ }
+}
+</script>
+
+<style scoped>
+h1{
+    color: orange;
+}
+</style>
