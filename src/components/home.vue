bce5a24410f480b206e5074659497830fcbcfd71
Your Name <you@example.com>
2022-07-21 17:58:31 +0530

    home updated

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,33 +4,46 @@
 <h1>Mobile ....!{{Mobile}}</h1>
 <h1>Mobile ....!{{Mobile==8888}}</h1>
 <h1>company....!{{getName('geoSpoc')}}</h1> -->
-<h1 v-on:mousemove="getConsole()">Home</h1>
+<!-- <h1 v-on:mousemove="getConsole()">Home</h1>
 <button v-on:click="getData()">Click Me!</button>
 
-<h2>{{count}}</h2>
+<h2>{{count}}</h2> -->
+<h2>pass data to child</h2>
+<ChildPage name="ankita" :user="user" :getData="getData"/>
 
 </template>
 
 <script>
+import ChildPage from './childPage.vue'
 export default {
-    name:'homePage',
-    data()
-    {
-         return{
-            count:0
-         }
-    },
+    name: "homePage",
+       components: { ChildPage },
+    data() {
+        return {
+            /*  count:0 */
+            user:
+        
+        {
+            name: "peter",
+                email:
+            "ankia@est.com"
+        }
+    }
+ 
+},
     methods:{
 getData()
 {
-  this.count=this.count+1;
-},
-getConsole()
+    console.log("parent function called")
+//   this.count=this.count+1;
+}
+/* getConsole()
 {
     console.warn("function called");
-}
+} */
  }
 }
+
 </script>
 
 <style scoped>
